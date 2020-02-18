d20182adb4306c2947c61a6137a24a09407cf599
Karn Tiwari <53399509+Karn-Tiwari-3003@users.noreply.github.com>
2020-02-18 22:21:58 +0530

    Update Kruskall's Algorithm.cpp

--- Graphs2/Kruskall's Algorithm.cpp
+++ Graphs2/Kruskall's Algorithm.cpp
@@ -29,84 +29,69 @@ Sample Input 1 :
 2 3 8
  */
 
-
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 class Edge{
     public:
-    int weight;
-    int first;
-    int second;
-    
-    Edge(){
-        weight=0;
-        first=0;
-        second=0;
-    }
-    
-    Edge(int w,int f,int s){
-        weight=w;
-        first=f;
-        second=s;        
-    }
-    bool operator <(const Edge &e1) const{
-        return weight>=e1.weight;
-    }
+    int src, des, weight;
 };
+
+bool compare(Edge e1, Edge e2){
+    return e1.weight < e2.weight;
+}
+
+int getParent(int node, int* parent){
+    if(node != parent[node])
+        return getParent(parent[node], parent);
+    return node;
+}
+
+Edge* kruskal(Edge* edge, int n, int E){
+    Edge* output = new Edge[n-1];
+    int count = 0;
+    int i = 0;
+    
+    int* parent = new int[n];
+    for(int i = 0 ; i < n ; i++){
+        parent[i] = i;
+    }
+    
+    while(count < (n-1)){
+        int srcParent = getParent(edge[i].src, parent);
+        int desParent = getParent(edge[i].des, parent);
+        if(srcParent != desParent){
+            output[count] = edge[i];
+            count += 1;
+            parent[srcParent] = desParent; 
+        }
+        i += 1;
+    }
+    return output;
+}
 
 int main()
 {
-    int V, E;
-    cin >> V >> E;
+    int n, E;
+    cin >> n >> E;
+    Edge* edge = new Edge[E];
     
-    priority_queue<Edge> pq;
-    
-    vector <Edge> MST;
-    
-    for(int i=0;i<E;i++){
-        int ei,ej,wi;
-        cin>>ei>>ej>>wi;
-        
-        Edge E(wi,ei,ej);
-        pq.push(E);
+    for(int i = 0 ; i < E ; i++){
+        int s, d, w;
+        cin >> s >> d >> w;
+        edge[i].src = s;
+        edge[i].des = d;
+        edge[i].weight = w;
     }
     
-    int count=0;
-    int *parent=new int[V];
+    sort(edge, edge + E, compare);
+    Edge* output = kruskal(edge, n, E);
     
-    for(int i=0;i<V;i++)
-        parent[i]=i;
-    
-    while(count<V-1 && !pq.empty()){
-        Edge e=pq.top();
-        pq.pop();
-        
-        int x1=parent[e.first];
-        
-        while(x1!=parent[x1])
-            x1=parent[x1];
-        
-        int x2=parent[e.second];
-        while(x2!=parent[x2])
-            x2=parent[x2];
-        
-        if(x1!=x2){
-            count++;
-            MST.push_back(e);
-            
-            parent[x1]=x2;
-        }
+    for(int i = 0 ; i < n-1 ; i++){
+        if(output[i].src < output[i].des)
+            cout << output[i].src << " " << output[i].des << " " << output[i].weight << endl;
+        else
+            cout << output[i].des << " " << output[i].src << " " << output[i].weight << endl;
     }
-    
-    for(int i=0;i<MST.size();i++){
-        
-        int a=(MST[i].first<=MST[i].second)?MST[i].first:MST[i].second;
-        int b=(MST[i].first>MST[i].second)?MST[i].first:MST[i].second;
-        
-        cout<<a<<" "<<b<<" "<<MST[i].weight<<endl;;
-    }
-
-  return 0;
+    return 0;
 }
