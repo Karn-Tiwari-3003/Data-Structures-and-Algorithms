661231e1f948b40c2058dc075dd79c94b739ed21
Karn Tiwari <53399509+Karn-Tiwari-3003@users.noreply.github.com>
2020-02-19 18:51:53 +0530

    Update PrimsAlgo.cpp

--- Graphs2/PrimsAlgo.cpp
+++ Graphs2/PrimsAlgo.cpp
@@ -29,83 +29,80 @@ Sample Input 1 :
 2 3 8
  */
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int getMinVertex(bool*visited,int* weight,int V)
-{
-    int minVertex=-1;
-    for(int i=0;i<V;i++)
-    {
-        if(!visited[i] && ((minVertex==-1)||(weight[minVertex]>weight[i])))
-        {
-            minVertex=i;
+
+int getMin(int* weight, bool* visited, int n){
+    int min = INT_MAX, min_index;
+    
+    for(int i = 0 ; i < n ; i++){
+        if(!visited[i] && weight[i] < min){
+            min_index = i;
+            min = weight[i];
         }
     }
-    return minVertex;
+    return min_index;
 }
-void prims(int** edges,int V)
-{
-    int* parent=new int[V];
-    int* weight=new int[V];
-    bool* visited=new bool[V];
-    for(int i=0;i<V;i++)
-    {
-        visited[i]=false;
-        weight[i]=INT_MAX;
+
+void printMst(int** edge, int* parent, int n){
+    for(int i = 1 ; i < n ; i++){
+        if(i < parent[i])
+            cout << i << " " << parent[i] << " " << edge[i][parent[i]] << endl;
+        else
+            cout << parent[i] << " " << i << " " << edge[i][parent[i]] << endl;
     }
-    weight[0]=0;
-    parent[0]=-1;
-    for(int i=0;i<V;i++)
-    {
-        int mini=getMinVertex(visited,weight,V);
-        visited[mini]=true;
-        for(int j=0;j<V;j++)
-        {
-            if(edges[mini][j]!=0)
-            {
-               if(weight[j]>edges[mini][j] && !visited[j])
-               {
-                   weight[j]=edges[mini][j];
-                   parent[j]=mini;
-               }
+}
+
+void prim(int** edge, int n, int E){
+    int* parent = new int[n];
+    int* weight = new int[n];
+    bool* visited = new bool[n];
+    for(int i = 0 ; i < n ; i++){
+        parent[i] = -1;
+        weight[i] = INT_MAX;
+        visited[i] = false;
+    }
+
+    weight[0] = 0;
+    
+    for(int j = 0 ; j < n-1 ; j++){
+        int curr_index = getMin(weight, visited, n);
+        visited[curr_index] = true;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(edge[curr_index][i] && !visited[i] && edge[curr_index][i] < weight[i]){
+                weight[i] = edge[curr_index][i];
+                parent[i] = curr_index;
             }
         }
     }
-    for(int i=1;i<V;i++)
-    {
-        if(parent[i]<i)
-        {
-            cout<<parent[i]<<" "<<i<<" "<<weight[i]<<endl;
-        }
-        else
-        {
-            cout<<i<<" "<<parent[i]<<" "<<weight[i]<<endl;
-        }
-    }
-    delete [] parent;
-    delete [] weight;
-    delete [] visited;
+    
+    printMst(edge, parent, n);
 }
+
 int main()
 {
-  int V, E;
-  cin >> V >> E;
-  int** edges=new int*[V];
-    for(int i=0;i<V;i++)
-    {
-        edges[i]=new int[V];
-        for(int j=0;j<V;j++)
-        {
-            edges[i][j]=0;
-        }
+    int n, E;
+    cin >> n >> E;
+    int** edges = new int*[n];
+    for(int i = 0 ; i < n ; i++){
+        edges[i] = new int[n];
+        for(int j = 0 ; j < n ; j++)
+            edges[i][j] = 0;
     }
-    for(int i=0;i<E;i++)
-    {
-        int f,s,w;
-        cin>>f>>s>>w;
-        edges[f][s]=w;
-        edges[s][f]=w;
+    
+    for(int i = 0 ; i < E ; i++){
+        int s, d, w;
+        cin >> s >> d >> w;
+        edges[s][d] = w;
+        edges[d][s] = w;
     }
-    prims(edges,V);
-  return 0;
+    
+    prim(edges, n, E);
+    
+    for(int i = 0 ; i < n ; i++){
+        delete[] edges[i];
+    }
+    delete[] edges;
+    return 0;
 }
