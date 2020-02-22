579acbee8197389ba0f9cdd88152f41f9e9891bc
Karn Tiwari <53399509+Karn-Tiwari-3003@users.noreply.github.com>
2020-02-22 13:59:16 +0530

    Update Bottom.cpp

--- Graphs2/Bottom.cpp
+++ Graphs2/Bottom.cpp
@@ -26,150 +26,117 @@ Sample Output
 1 3
 2
  */
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
 using namespace std;
 
-
-typedef unordered_map<int,vector<int> > UM;
-
-void init_vis(bool visited[],int v){
-    for(int i=1;i<=v;i++)
-        visited[i]=false;
-}
-
-void DFSUtil_StackFill(vector<int>adj[],int node,stack<int> &sf,bool* visited){
-
-    visited[node]=true;
-
-    for(int i=0;i<adj[node].size();i++)
-    {
-        int newnode=adj[node][i];
-        if(!visited[newnode])
-            DFSUtil_StackFill(adj,newnode,sf,visited);
+void dfs(vector<int>* edges, stack<int>& finishedVertices, bool* visited, int n, int start){
+    visited[start] = true;
+    for(int i = 0 ; i < edges[start].size() ; i++){
+        int node = edges[start][i];
+        if(!visited[node]){
+            dfs(edges, finishedVertices, visited, n, node);
+        }
     }
-    sf.push(node);
+    finishedVertices.push(start);
 }
 
-void DFSUtil_reverse(vector<int>adjr[],int node,bool* visited,UM &SCC,int k){
+void dfs_reverse(vector<int>* edgesT, bool* visited, unordered_map<int,vector<int>>& SCC, int node, int k){
     SCC[k].push_back(node);
-    visited[node]=true;
-
-    for(int i=0;i<adjr[node].size();i++)
-    {
-        int newnode=adjr[node][i];
-        if(!visited[newnode])
-            DFSUtil_reverse(adjr,newnode,visited,SCC,k);
+    visited[node] = true;
+    
+    for(int i = 0 ; i < edgesT[node].size() ; i++){
+        int new_node = edgesT[node][i];
+        if(!visited[new_node]){
+            dfs_reverse(edgesT, visited, SCC, new_node, k);
+        }
     }
-
 }
 
-
-//This funtion will get me SCC of the graph using above two utilities
-UM kosarju(vector <int> adj[],vector <int> adjr[],int v,int e){
-
-    //Prepare Stack for finish time and the visited node
-    stack<int> sf;
-    bool visited[v+1];
-
-    init_vis(visited,v);
+void getSCC(vector<int>* edges, vector<int>* edgesT, int n){
+    bool* visited = new bool[n];
+    memset(visited, false, sizeof(visited));
     
-    //Fill the stack
-    for(int i=1;i<=v;i++)
-    {
-        if(!visited[i])
-            DFSUtil_StackFill(adj,i,sf,visited);
+    stack<int> finishedVertices;
+    for(int i = 0 ; i < n ; i++){
+        if(!visited[i]){
+            dfs(edges, finishedVertices, visited, n, i);
+        }
     }
-
-    init_vis(visited,v);
-
-    UM SCC;
-    int k=0;
-    while(!sf.empty())
-    {
-        int node=sf.top();
-        sf.pop();
-
-        if(!visited[node])
-        {
-            DFSUtil_reverse(adjr,node,visited,SCC,k);
+    
+    unordered_map<int,vector<int>> SCC;
+    int k = 0;
+    
+    for(int i = 0 ; i < n ; i++){
+        visited[i] = false;
+    }
+    
+    while(!finishedVertices.empty()){
+        int node = finishedVertices.top();
+        finishedVertices.pop();
+        
+        if(!visited[node]){
+            dfs_reverse(edgesT, visited, SCC, node, k);
             k++;
         }
     }
-    return SCC;
-
+    
+    int flag = 1;
+    vector<int> ans;
+    vector<int> bottom;
+    for(int i = 0 ; i < k ; i++){
+        for(int j = 0 ; j < SCC[i].size(); j++){
+            ans.push_back(SCC[i][j]);
+        }
+        for(int m = 0 ; m < ans.size() ; m++){
+            int node = ans[m];
+            for(int j = 0 ; j < edges[node].size() ; j++){
+                int new_node = edges[node][j];
+                vector<int> :: iterator it;
+                it = find(ans.begin(), ans.end(), new_node);
+                if(it == ans.end()){
+                    flag = 0;
+                    break;
+                }
+            }
+            if(flag == 0)
+                break;
+        }
+        if(flag == 1){
+            for(int j = 0 ; j < ans.size() ; j++)
+                bottom.push_back(ans[j]);
+        }
+        flag = 1;
+        ans.clear();
+    }
+    sort(bottom.begin(), bottom.end());
+    for(int i = 0 ; i < bottom.size() ; i++)
+        cout << bottom[i] + 1 << " ";
+    cout << endl;
 }
 
-//Constructs newvector 'a' by vector b to vector a
-void constrV(vector<int> &a,vector<int> &b){
-    a.insert(a.end(), b.begin(), b.end());
-}
-
-void printV(vector<int> &v){
-    sort(v.begin(),v.end());
-
-    for(int i=0;i<v.size();i++)
-        cout<<v[i]<<" ";
-    cout<<endl;
-}
-
-int main()
-{
-	//code
-    while(true)
-    {
-        int v,e;
-        cin>>v;
-        if(v==0)
+int main(){
+    while(true){
+        int n;
+        cin >> n;
+        if(n == 0)
             break;
         
-        cin>>e;
-        
-        //Prepare two graphs, one orignal and one reverse
-        vector <int> adj[v+1];
-        vector<int> adj_reversed[v+1];
-
-        for(int i=0;i<e;i++)
-        {
-            int x,y;
-            cin>>x>>y;
-            adj[x].push_back(y);
-            adj_reversed[y].push_back(x);
+        vector<int>* edges = new vector<int>[n];
+        vector<int>* edgesT = new vector<int>[n];
+        int e;
+        cin >> e;
+        for(int i = 0 ; i < e ; i++){
+            int x, y;
+            cin >> x >> y;
+            edges[x-1].push_back(y-1);
+            edgesT[y-1].push_back(x-1);
         }
-
-        UM SCC=kosarju(adj,adj_reversed,v,e);
-
-        //Prepare SCC mapping
-        unordered_map<int,int> sc;
-        for(int i=0;SCC[i].size()!=0;i++)
-        {
-            for(int j=0;j<SCC[i].size();j++)
-                sc[SCC[i].at(j)]=i;
-        }
-
-        //Check for outgoing edges for all nodes in a SCC
-        vector <int> BOTTOM;
-        for(int i=0;SCC[i].size()!=0;i++)
-        {
-            int flag=0;
-            for(int j=0;j<SCC[i].size();j++)
-            {
-                int nd=SCC[i][j];
-                for(int k=0;k<adj[nd].size();k++)
-                {
-                    if(sc[adj[nd][k]]!=i)
-                        flag=1;
-                }
-                if(flag)
-                    break;
-            }
-            if(flag==0)
-                constrV(BOTTOM,SCC[i]);
-                
-        }
-        printV(BOTTOM);
-
-        
-    }
     
-	return 0;
+        getSCC(edges, edgesT, n);
+    
+        delete [] edges;
+        delete [] edgesT;
+    }
+    return 0;
 }
